65b0ffc4fbf62efdde0bb7717fab1dac170a6926
GianmarcoGioia <gianmarco.gioa2@gmail.com>
2023-04-14 17:17:26 +0200

    minor changes

---
+++ assets/js/scriptPageScroll.js
@@ -0,0 +1,16 @@
+
+$(function() {
+  $.scrollify({
+      section : ".section",
+      easing: "easeOutCubic",
+      scrollSpeed: 600,
+  });
+});
+
+
+/*
+$('.link').click(function(){
+    var moveTo = $(this).attr('href');
+    $.scrollify("move",moveTo);
+  });
+  */
