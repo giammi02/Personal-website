296f2fb09fa4a60c0e1f19c13c323cf10486e566
GianmarcoGioia <gianmarco.gioa2@gmail.com>
2023-05-17 23:32:20 +0200

    submit message added

--- assets/js/scriptContact.js
+++ assets/js/scriptContact.js
@@ -1,42 +1,31 @@
-
-(function() {
+//initialization emailJS service
+(function () {
     emailjs.init('_HWVM4cS9nTqwC33w');
 })();
 
-window.onload = function() {
-    if(validationForm())
-    startEmailJS();
-    else{
 
-    }
-
-}
-
-/* **** Email JS code snippet ****
-https://www.emailjs.com/docs/tutorial/creating-contact-form/ */
-function startEmailJS(){
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    let submitMessage = document.querySelector("[data-submit-message]");
         // generate a five digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
         emailjs.sendForm('service_001', 'template_02xj607', this)
-            .then(function() {
-                console.log('SUCCESS!');
-            }, function(error) {
-                console.log('FAILED...', error);
+            .then(function () {
+                let name = document.querySelector("[data-name]");
+                let email = document.querySelector("[data-email]");
+                let message = document.querySelector("[data-message]");
+                //clear inputs
+                name.value="";
+                email.value="";
+                message.value="";
+
+                submitMessage.style.cssText= "color: rgba(240, 240, 240)";
+                submitMessage.innerHTML="The message has been successfully sent!";
+            }, function (error) {
+                submitMessage.style.cssText= "color: rgba(252, 39, 39, 0.9)";
+                submitMessage.innerHTML="Sorry, a problem occurred, the message hasn't been sent!";
+                submitMessage.scrollIntoView();
             });
     });
-}
-//Validation contact form  https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email#validation
-function validationForm(){
-let name = document.querySelector("[data-name]");
-let email = document.querySelector("[data-email]");
-let message = document.querySelector("[data-message]");
-
-
-
-}
-
-
 
